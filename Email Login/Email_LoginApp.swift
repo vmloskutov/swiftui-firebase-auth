2a51add008ff79001b1478f2a2281aeee92136c1
Vladimir Loskutov <vladimir@altermeliora.com>
2024-03-04 02:05:20 +0300

    Initial Commit

---
+++ Email Login/Email_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Email_LoginApp.swift
+//  Email Login
+//
+//  Created by Vladimir 3 on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Email_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
