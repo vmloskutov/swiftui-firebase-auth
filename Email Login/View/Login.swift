c3696c115d7a03d3257c24e340123df27eb7b725
Vladimir Loskutov <vladimir@altermeliora.com>
2024-03-04 23:36:33 +0300

    Init

---
+++ Email Login/View/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Email Login
+//
+//  Created by Vladimir 3 on 04.03.2024.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
